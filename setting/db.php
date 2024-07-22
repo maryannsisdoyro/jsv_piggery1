1ad48ad713a38711fd9195b1b6f32f7737b739ce
chadrhino <chadrhino29@gmail.com>
2024-07-22 11:13:23 +0800

    Update db.php

--- setting/db.php
+++ setting/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$db = new PDO('mysql:host=localhost;dbname=pig','root','');
+	$db = new PDO('mysql:host=localhost;dbname=u510162695_pig','u510162695_pig','1Pigdatabase');
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	die('<h4 style="color:red">Incorrect Connection Details</h4>');
