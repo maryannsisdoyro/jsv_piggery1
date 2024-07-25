0b1ca4350cce38949a83ee96baf366a37a7e2c98
chadrhino <chadrhino29@gmail.com>
2024-07-25 17:37:42 +0800

    Update db.php

--- setting/db.php
+++ setting/db.php
@@ -4,11 +4,11 @@ define('DB_HOST', 'localhost'); // Changed to localhost
 define('DB_USER', 'u510162695_pig');
 define('DB_PASS', '1Pigdatabase');
 define('DB_NAME', 'u510162695_pig');
-define('DB_PORT', '3306'); // Note: Changed to DB_PORT to align with typical naming conventions
+// define('DB_PORT', '3306'); // Note: Changed to DB_PORT to align with typical naming conventions
 
 // Attempt to connect to the database
 try {
-    $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT;
+    $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
     $db = new PDO($dsn, DB_USER, DB_PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
