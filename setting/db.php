9d93ffafbdd5c0a4eae79fd1128d4b02f40e70ff
chadrhino <chadrhino29@gmail.com>
2024-07-25 16:51:11 +0800

    Update db.php

--- setting/db.php
+++ setting/db.php
@@ -11,7 +11,6 @@ try {
     $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT;
     $db = new PDO($dsn, DB_USER, DB_PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('<h4 style="color:red"></h4>');
-}
+} 
 ?>
+
