f763bbc14a6e1236e7bf5c7c614489d9798bd470
chadrhino <chadrhino29@gmail.com>
2024-08-01 21:40:09 +0800

    Update db.php

--- setting/db.php
+++ setting/db.php
@@ -2,9 +2,9 @@
 // Define constants for database connection details
 define('DB_HOST', '127.0.0.1');
 
-// define('DB_USER', 'u510162695_pig');
-// define('DB_PASS', '1Pigdatabase');
-// define('DB_NAME', 'u510162695_pig');
+define('DB_USER', 'u510162695_pig');
+define('DB_PASS', '1Pigdatabase');
+define('DB_NAME', 'u510162695_pig');
 
 // define('DB_USER', 'root');
 // define('DB_PASS', '');
